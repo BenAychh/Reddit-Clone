5b970f079ee9fcc1ed6d51b096288d4c423a0a17
BenAychh <ben.tomas.hernandez@gmail.com>
2016-04-01 21:10:34 -0600

    Basic post adding form, no functionality

--- public/javascripts/master.js
+++ public/javascripts/master.js
@@ -113,7 +113,18 @@ app.controller('thePosts', function($scope, postFactory) {
   $scope.postClass = postFactory;
   $scope.posts = $scope.postClass.get();
   $scope.reverseSort = false;
+  $scope.showAddPost = false;
   $scope.flipSort = function() {
     $scope.reverseSort = !$scope.reverseSort;
   }
+  $scope.fileChanged = function(element) {
+    var photofile = element.files[0];
+    var reader = new FileReader();
+    reader.onload = function(e) {
+      $scope.$apply(function() {
+        $scope.prevImg = e.target.result;
+      });
+    };
+    reader.readAsDataURL(photofile);
+  };
 });
